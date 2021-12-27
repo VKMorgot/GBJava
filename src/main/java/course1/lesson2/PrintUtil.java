821175aa24d0b00034b5767735e02fa966f22173
VKMorgot <solerosa@mail.ru>
2021-12-27 03:41:06 +0300

    lesson1-2

---
+++ src/main/java/course1/lesson2/PrintUtil.java
@@ -0,0 +1,13 @@
+package course1.lesson2;
+
+public class PrintUtil {
+
+    /**
+     * Форматирует строку тремя * до и после
+     * @param str строка для форматирования
+     */
+    public static void printFormatted(String str) {
+        System.out.println("***" + str + "***");
+    }
+
+}
