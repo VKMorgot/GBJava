dc3fcf4f4f19c45ee81fd7463dcd6a5e635334fe
VKMorgot <solerosa@mail.ru>
2021-12-30 02:48:52 +0300

    задание №2: определение знака числа Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль, положительное ли число передали или отрицательное. Замечание: ноль считаем положительным числом.

--- src/main/java/course1/lesson2/homework/HomeWorkApp2.java
+++ src/main/java/course1/lesson2/homework/HomeWorkApp2.java
@@ -4,46 +4,70 @@ import java.util.Random;
 
 public class HomeWorkApp2 {
 
-    /**
-     * Задание №1
-     * Проверка, что сумма a + b лежит в пределах от 10 до 20 (включительно)
-     * @param a целое число
-     * @param b целое число
-     * @return true or false
-     */
-    public static boolean checkSum (int a, int b) {
-        return ((a + b) > 10) && ((a + b) <= 20);
-    }
-
-    /**
-     * Выбор случайного целого числа в заданном интервале
-     * @param min минимальная граница интервала
-     * @param max максимальная граница интервала
-     * @return случайное целое число
-     */
-    public static int getRandomNumber (int min, int max) {
-        Random random = new Random();
-        return random.nextInt(max - min) + min;
-    }
-
     public static void main(String[] args) {
         // вызов метода проверки суммы
         //System.out.println(checkSum(getRandomNumber(-5, 20), getRandomNumber(-5, 20)));
 
         int a = getRandomNumber(-5, 20);
         int b = getRandomNumber(-5, 20);
-        String s = "";
+        String str = "";
 
         if (!checkSum(a, b)) {
-            s = " не";
+            str = " не";
         }
 
         System.out.println("Проверка суммы");
-        System.out.printf("Сумма чисел %d и %d%s лежит в интервале от 10 до 20", a, b, s);
-        System.out.println();
+        System.out.printf("Сумма чисел %d и %d%s лежит в интервале от 10 до 20", a, b, str);
 
+        separator();
+        System.out.println("Проверка числа");
+        isPositive(getRandomNumber(-100, 100));
 
 
     }
 
+    /**
+     * Задание №2
+     * Печать в консоль, какое число было передано: положительное или отрицательное
+     * @param a целое число, ноль считаем положительным числом
+     */
+    public static void isPositive (int a) {
+        if (a >= 0) {
+            System.out.printf("Число %d положительное", a);
+        } else {
+            System.out.printf("Число %d отрицательное", a);
+        }
+    }
+
+    /**
+     * Задание №1
+     * Проверка, что сумма a + b лежит в пределах от 10 до 20 (включительно)
+     *
+     * @param a целое число
+     * @param b целое число
+     * @return true or false
+     */
+    public static boolean checkSum(int a, int b) {
+        return ((a + b) > 10) && ((a + b) <= 20);
+    }
+
+    /**
+     * Выбор случайного целого числа в заданном интервале
+     *
+     * @param min минимальная граница интервала
+     * @param max максимальная граница интервала
+     * @return случайное целое число
+     */
+    public static int getRandomNumber(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min) + min;
+    }
+
+    /**
+     * Метод для разделения вывода выполненных пунктов задания
+     */
+    public static void separator() {
+        System.out.println();
+        System.out.println();
+    }
 }
