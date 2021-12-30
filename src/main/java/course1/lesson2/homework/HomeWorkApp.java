2d8e357321d2f9215d1b5235931667b9f249ebe3
VKMorgot <solerosa@mail.ru>
2021-12-30 11:23:47 +0300

    задание №4: вывод строки в консоль Написать метод, которому в качестве аргументов передается строка и число, метод должен отпечатать в консоль указанную строку, указанное количество раз;

--- src/main/java/course1/lesson2/homework/HomeWorkApp.java
+++ src/main/java/course1/lesson2/homework/HomeWorkApp.java
@@ -4,6 +4,82 @@ import java.util.Random;
 
 public class HomeWorkApp {
 
+    /**
+     * Выбор случайного целого числа в заданном интервале
+     *
+     * @param min минимальная граница интервала
+     * @param max максимальная граница интервала
+     * @return случайное целое число
+     */
+    public static int getRandomNumber(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min) + min;
+    }
+
+    /**
+     * Метод для разделения вывода выполненных пунктов задания
+     */
+    public static void separator() {
+        System.out.println();
+        System.out.println();
+    }
+
+    interface Printable {
+        String print(boolean bool);
+    }
+
+    /**
+     * Задание №1
+     * Проверка, что сумма a + b лежит в пределах от 10 до 20 (включительно)
+     *
+     * @param a целое число
+     * @param b целое число
+     * @return true or false
+     */
+    public static boolean checkSum(int a, int b) {
+        return ((a + b) > 10) && ((a + b) <= 20);
+    }
+
+    /**
+     * Задание №2
+     * Печать в консоль, какое число было передано: положительное или отрицательное
+     *
+     * @param a целое число, ноль считаем положительным числом
+     */
+    public static void isPositive(int a) {
+        if (a >= 0) {
+            System.out.printf("Число %d положительное", a);
+        } else {
+            System.out.printf("Число %d отрицательное", a);
+        }
+    }
+
+    /**
+     * Задание №3
+     * Определение знака числа
+     *
+     * @param a целое число, ноль считаем положительным числом
+     * @return true, если число отрицательное; false, если число положительное
+     */
+    public static boolean isNegative(int a) {
+        return (a < 0);
+    }
+
+    /**
+     * Задание №4
+     * Вывод в консоль строки str n-е число раз
+     *
+     * @param str строка для вывода
+     * @param n определяет число выводов строки, не должно быть отрицательным
+     */
+    public static void printString(String str, int n) {
+        if (n >= 0) {
+            for (int i = 0; i < n; i++) {
+                System.out.println(str);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // вызов метода проверки суммы
 
@@ -46,64 +122,10 @@ public class HomeWorkApp {
             str = "положительное";
         }
         System.out.printf("Число %d %s", c, str);
-    }
 
-    /**
-     * Задание №3
-     * Определение знака числа
-     * @param a целое число, ноль считаем положительным числом
-     * @return true, если число отрицательное; false, если число положительное
-     */
-    public static boolean isNegative (int a) {
-        return (a < 0);
-    }
-
-    /**
-     * Задание №2
-     * Печать в консоль, какое число было передано: положительное или отрицательное
-     * @param a целое число, ноль считаем положительным числом
-     */
-    public static void isPositive (int a) {
-        if (a >= 0) {
-            System.out.printf("Число %d положительное", a);
-        } else {
-            System.out.printf("Число %d отрицательное", a);
-        }
-    }
-
-    /**
-     * Задание №1
-     * Проверка, что сумма a + b лежит в пределах от 10 до 20 (включительно)
-     *
-     * @param a целое число
-     * @param b целое число
-     * @return true or false
-     */
-    public static boolean checkSum(int a, int b) {
-        return ((a + b) > 10) && ((a + b) <= 20);
-    }
-
-    /**
-     * Выбор случайного целого числа в заданном интервале
-     *
-     * @param min минимальная граница интервала
-     * @param max максимальная граница интервала
-     * @return случайное целое число
-     */
-    public static int getRandomNumber(int min, int max) {
-        Random random = new Random();
-        return random.nextInt(max - min) + min;
-    }
-
-    /**
-     * Метод для разделения вывода выполненных пунктов задания
-     */
-    public static void separator() {
-        System.out.println();
-        System.out.println();
-    }
-
-    interface Printable {
-        String print(boolean bool);
+        // вывод строки в консоль
+        separator();
+        int n = getRandomNumber(0, 10);
+        printString("Выводим строку " + n + " раз", n);
     }
 }
