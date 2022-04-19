326d4ce149503208e0d07e4ddb35806c63918471
VKMorgot <solerosa@mail.ru>
2022-04-19 16:23:19 +0300

    lesson2-4

---
+++ src/main/java/course2/lesson4/ChatServer.java
@@ -0,0 +1,12 @@
+package course2.lesson4;
+
+public class ChatServer {
+
+    public void start(int port) {
+        System.out.println("Server started at port " + port);
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+}
